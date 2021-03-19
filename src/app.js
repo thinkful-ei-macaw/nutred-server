dc2780b1691653a882efc3bd4c2ebf55235255f4
Brannen Petit <bpetit940@gmail.com>
2021-03-19 14:35:09 -0500

    added ACAO

--- src/app.js
+++ src/app.js
@@ -14,6 +14,14 @@ const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(cors());
 app.use(morgan(morganOption));
 app.use(helmet());
