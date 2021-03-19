64fa34aab55a2c3be69bd2c339e8a23e1e3003d5
Brannen Petit <bpetit940@gmail.com>
2021-03-19 18:10:39 -0500

    back to npm cors

--- src/app.js
+++ src/app.js
@@ -14,32 +14,9 @@ const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
-app.use(cors());
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors());
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", registerRouter);
