f2bc3b406d4a588ab97ddcc9402f54f7481dd13b
Brannen Petit <bpetit940@gmail.com>
2021-03-21 16:26:06 -0500

    added debug

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
+  DEBUG: true,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL || "postgresql://postgres@localhost/nutred-db",
