8653fc3e40d1f28a802a40534b3f76d7de001ca2
Brannen Petit <bpetit940@gmail.com>
2021-03-21 14:56:13 -0500

    cors attempt 9

--- src/app.js
+++ src/app.js
@@ -17,6 +17,9 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
+app.use((req, res, next) => {
+  res.header({"Access-Control-Allow-Origin": "*"});
+}); 
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", registerRouter);
