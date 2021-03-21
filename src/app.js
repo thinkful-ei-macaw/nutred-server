e896ada2ddff213d3d170ea6c876243e5e20365f
Brannen Petit <bpetit940@gmail.com>
2021-03-21 14:25:03 -0500

    updated port

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors({ origin: true }));
+app.use(cors());
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header(
