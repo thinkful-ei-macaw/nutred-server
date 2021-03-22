e84216bb7f752caed88f88b5e1f0052f561bdaaf
Brannen Petit <bpetit940@gmail.com>
2021-03-22 17:29:04 -0500

    added extra

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.get("/api/login", (req, res) => {});
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
+    response = { error: { message: "extra server error" } };
   } else {
     console.error(error);
     response = { message: error.message, error };
